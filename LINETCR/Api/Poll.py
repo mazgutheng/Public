143b6a7f1e6e0cdec1f4c41e200d7604ffca72fd
humanzone8 <35399824+humanzone8@users.noreply.github.com>
2018-01-16 22:10:15 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,9 +18,9 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.4.17"
+  UA = "Line/8.0.0"
 #  LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
-  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
+  LA = "CHROMEOS\t1.8.0.0\x09Chrome_OS\t1"
 
   rev = 0
 
